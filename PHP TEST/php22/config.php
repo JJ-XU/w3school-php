fb73299fa4b8773120aef5febb0f9f3c98ba7a53
xuwenjuan <1095553978@qq.com>
2017-05-20 12:49:47 +0800

    php519

---
+++ PHP TEST/php22/config.php
@@ -0,0 +1,10 @@
+<?php
+return $config=[
+    'db'=>'mysql',
+    'host'=>'localhost',
+    'username'=>'root',
+    'password'=>'root',
+    'charset'=>'UTF-8',
+    'port'=>'3306',
+    'dbname'=>'test'
+];
