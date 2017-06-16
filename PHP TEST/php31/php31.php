b8a5c9365dac312c9c77fd96123c72cc46acf6ba
xuwenjuan <1095553978@qq.com>
2017-06-17 02:09:40 +0800

    php0616

---
+++ PHP TEST/php31/php31.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP array</title>
+</head>
+<body>
+<?php
+/**
+ * @author 许文娟 <xuwenjuan@atlasdata.com.cn>
+ * @version 1.0
+ * @copyright 2014-2017 海量云图（北京）数据技术有限公司
+ */
+
+$cities[] = "<b>北京</b>";    //等同于$cities[0] = "北京"
+$cities[] = "<b>天津</b>";    //等同于$cities[1] = "天津"
+$cities[] = "<b>上海</b>";    //等同于$cities[2] = "上海"
+$cities[] = "<b>深圳</b>";    //等同于$cities[3] = "深圳"
+
+/*
+** 统计元素个数
+*/
+$indexLimit = count($cities);   //把数组中元素的个数赋给$indexLimit
+
+/*
+** 输出所有数组
+*/
+for ($index = 0; $index < $indexLimit; $index++) {
+    echo "第 $index 个城市是 $cities[$index]。 <br>";
+}
+
+?>
+</body>
+</html>
